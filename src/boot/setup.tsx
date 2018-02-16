b7e43eaaa8794a65c8e28071721375e7bc058538
akhil-geekyants <akhil@geekyants.com>
2018-02-16 15:03:02 +0530

    Added files after eject

--- src/boot/setup.tsx
+++ src/boot/setup.tsx
@@ -1,4 +1,3 @@
-import * as Expo from "expo";
 import * as React from "react";
 import { Provider } from "mobx-react/native";
 import { StyleProvider } from "native-base";
@@ -6,11 +5,11 @@ import { StyleProvider } from "native-base";
 import App from "../App";
 import getTheme from "../theme/components";
 import variables from "../theme/variables/platform";
-export interface Props {}
+export interface Props { }
 export interface State {
 	isReady: boolean;
 }
-export default function(stores) {
+export default function (stores) {
 	return class Setup extends React.Component<Props, State> {
 		state: {
 			isReady: boolean;
@@ -21,23 +20,7 @@ export default function(stores) {
 				isReady: false,
 			};
 		}
-		componentWillMount() {
-			this.loadFonts();
-		}
-		async loadFonts() {
-			await Expo.Font.loadAsync({
-				Roboto: require("native-base/Fonts/Roboto.ttf"),
-				Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-				Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-			});
-
-			this.setState({ isReady: true });
-		}
-
 		render() {
-			if (!this.state.isReady) {
-				return <Expo.AppLoading />;
-			}
 			return (
 				<StyleProvider style={getTheme(variables)}>
 					<Provider {...stores}>
